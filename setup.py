8fba26b4e67af663705abe959aafd5f108233a68
Hew Zhong Ken <zhongken.hew@ytlcomms.my>
2024-11-26 09:52:29 +0800

    change structure

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='RankGPT',
     version='0.1',
-    packages=find_packages(),
-    install_requires=['tqdm', 'openai', 'tiktoken', 'pyserini'],
+    packages=find_packages(),  # Automatically finds the 'rankgpt' package
+    install_requires=[
+        'tqdm',
+        'openai',
+        'tiktoken',
+        'pyserini',
+    ],
+    include_package_data=True,  # Optional: Include non-Python files
+    description="RankGPT: A ranking-focused GPT library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="",
+    url="https://github.com/HewZK1211/RankGPT",
 )
